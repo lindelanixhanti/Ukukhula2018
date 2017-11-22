43d0611c1c8e4ae6f51fb684790b3573224b654e
PromisT <nyamunda17@gmail.com>
2017-11-22 10:24:51 +0200

    fixed notification validations

--- app/src/main/java/za/co/codetribe/ukukhula/notifications/Eventhelper.java
+++ app/src/main/java/za/co/codetribe/ukukhula/notifications/Eventhelper.java
@@ -5,15 +5,19 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -35,29 +39,25 @@ import java.util.Calendar;
 import java.util.List;
 
 
-
+import za.co.codetribe.ukukhula.AdminProfile.ProfileActivity;
+import za.co.codetribe.ukukhula.MainActivity;
 import za.co.codetribe.ukukhula.R;
 import za.co.codetribe.ukukhula.gallery.GallaryActivity;
 import za.co.codetribe.ukukhula.gallery.ImageDisplayActivity;
 
 import static android.app.DatePickerDialog.*;
 
-/**
- * Created by Codetribe on 2017/09/01.
- */
-
-
 public class Eventhelper extends AppCompatActivity {
 
     EditText Ename, Edescription;
-    TextView  select;
+    EditText  select;
     String eName, eDescription,eDate;
     Button save;
     Boolean val = true;
     private static final String TAG=" HJHJKHJJH";
 
 
-
+    FloatingActionButton add;
     FirebaseDatabase firebaseData;
     DatabaseReference roofdef, demodef;
     ListView listview;
@@ -66,27 +66,17 @@ public class Eventhelper extends AppCompatActivity {
     DatabaseReference mDatabase;
     FirebaseAuth mAuth;
     FirebaseUser user;
+    private CalendarView mCalendarView;
 
     private DatePickerDialog.OnDateSetListener mDateSetListener;
 
 
 
     //ProfileActivityhelpers helper;
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-                return super.onOptionsItemSelected(item);
-
-        }
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_events);
+        setContentView(R.layout.activity_eventhelper);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -94,7 +84,7 @@ public class Eventhelper extends AppCompatActivity {
         save = (Button) findViewById(R.id.saveData);
         Ename = (EditText) findViewById(R.id.eventName);
         Edescription = (EditText) findViewById(R.id.eventDescription);
-        select=(TextView)findViewById(R.id.date);
+        select = (EditText) findViewById(R.id.date);
 
 
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Events");
@@ -102,104 +92,127 @@ public class Eventhelper extends AppCompatActivity {
         eventList = new ArrayList<>();
         listview = (ListView) findViewById(R.id.listView);
 
-        select.setOnClickListener(new View.OnClickListener() {
+//        select.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Calendar calendar= Calendar.getInstance();
+//                int year=calendar.get(Calendar.YEAR);
+//                int day=calendar.get(Calendar.DAY_OF_MONTH);
+//                int month=calendar.get(Calendar.MONTH);
+//
+//                DatePickerDialog dialog= new DatePickerDialog(Eventhelper.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,mDateSetListener,year,month,day);
+//                dialog.getWindow().setBackgroundDrawable(new ColorDrawable((Color.TRANSPARENT)));
+//                dialog.show();
+//
+//
+//            }
+//        });
+//        mDateSetListener= new DatePickerDialog.OnDateSetListener() {
+//            @Override
+//            public void onDateSet(DatePicker datePicker, int year, int month, int date) {
+//
+//              month=month+1;
+//                String selected= month + "/" + date +" / " + year;
+//
+//                select.setText(selected);
+//            }
+//        };
+
+
+//        save.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                addEvent();
+//            }
+//        });
+        add = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Calendar calendar= Calendar.getInstance();
-                int year=calendar.get(Calendar.YEAR);
-                int day=calendar.get(Calendar.DAY_OF_MONTH);
-                int month=calendar.get(Calendar.MONTH);
+                //Toast.makeText(getBaseContext(), "I was clicked", Toast.LENGTH_LONG).show();
+//                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+//                galleryIntent.setType("image/*");
+//                startActivityForResult(galleryIntent, ACTION_CODE);
 
-                DatePickerDialog dialog= new DatePickerDialog(Eventhelper.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,mDateSetListener,year,month,day);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable((Color.TRANSPARENT)));
-                dialog.show();
-
+                Intent intent = new Intent(Eventhelper.this, EventActivity.class);
+                startActivity(intent);
+            }
+        });
+        mCalendarView = (CalendarView) findViewById(R.id.calendarView);
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView CalendarView, int year, int month, int dayOfMonth) {
+                String date = year + "/" + month + "/" + dayOfMonth;
+                Log.d(TAG, "onSelectedDayChange: yyyy/mm/dd:" + date);
+                Intent intent = new Intent(Eventhelper.this, Event.class);
+                intent.putExtra("date", date);
+                startActivity(intent);
 
             }
         });
-        mDateSetListener= new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int date) {
 
-              month=month+1;
-                String selected= month + "/" + date +"  " + year;
+//    private void addEvent() {
+//        Event event = new Event();
+//        eName = Ename.getText().toString().trim();
+//        eDescription = Edescription.getText().toString().trim();
+//        eDate=select.getText().toString().trim();
+//
+//
+//
+//        if (!TextUtils.isEmpty(eName)) {
+//            event.setEventName(eName);
+//
+//
+//        } else {
+//            Toast.makeText(Eventhelper.this, "Event not saved ", Toast.LENGTH_LONG).show();
+//        }
+//
+//
+//
+//        if (!TextUtils.isEmpty(eDescription)) {
+//            event.setEventDiscription(eDescription);
+//
+//
+//        } else {
+//            Toast.makeText(Eventhelper.this, "no desc", Toast.LENGTH_LONG).show();
+//        }
+//
+//
+//        if (!TextUtils.isEmpty(eDate)) {
+//            event.setDate(eDate);
+//
+//
+//        } else {
+//            Toast.makeText(Eventhelper.this, "NO DATE", Toast.LENGTH_LONG).show();
+//        }
+//
+//
+//        Event eve = new Event(eName, eDescription,eDate);
+//        mDatabase.push().setValue(eve);
+//
+//        Ename.setText(" ");
+//        Edescription.setText(" ");
+//        select.setText("");
+//
+//        try {
+//
+//
+//        } catch (Exception e) {
+//            val = false;
+//        } finally {
+//            if (val) {
+//                Toast.makeText(Eventhelper.this, "Event saved ", Toast.LENGTH_LONG).show();
+//            } else {
+//                Toast.makeText(Eventhelper.this, "Event not saved ", Toast.LENGTH_LONG).show();
+//            }
+//        }
+//
+//    }
+//
 
-                select.setText(selected);
-            }
-        };
 
-
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addEvent();
-            }
-        });
-
+        //fetch
     }
-
-    private void addEvent() {
-        Event event = new Event();
-        eName = Ename.getText().toString().trim();
-        eDescription = Edescription.getText().toString().trim();
-        eDate=select.getText().toString().trim();
-
-
-
-        if (!TextUtils.isEmpty(eName)) {
-            event.setEventName(eName);
-
-
-        } else {
-            Toast.makeText(Eventhelper.this, "Event not saved ", Toast.LENGTH_LONG).show();
-        }
-
-
-
-        if (!TextUtils.isEmpty(eDescription)) {
-            event.setEventDiscription(eDescription);
-
-
-        } else {
-            Toast.makeText(Eventhelper.this, "no desc", Toast.LENGTH_LONG).show();
-        }
-
-
-        if (!TextUtils.isEmpty(eDate)) {
-            event.setDate(eDate);
-
-
-        } else {
-            Toast.makeText(Eventhelper.this, "NO DATE", Toast.LENGTH_LONG).show();
-        }
-
-
-        Event eve = new Event(eName, eDescription,eDate);
-        mDatabase.push().setValue(eve);
-
-        Ename.setText(" ");
-        Edescription.setText(" ");
-        select.setText("");
-
-        try {
-
-
-        } catch (Exception e) {
-            val = false;
-        } finally {
-            if (val) {
-                Toast.makeText(Eventhelper.this, "Event saved ", Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(Eventhelper.this, "Event not saved ", Toast.LENGTH_LONG).show();
-            }
-        }
-
-    }
-
-
-
-    //fetch
-
     @Override
     protected void onStart() {
         super.onStart();
@@ -230,8 +243,18 @@ public class Eventhelper extends AppCompatActivity {
         });
 
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
-
+        finish();
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 //    public void showUpdateDIALOG(String id, String eventName, String eventDescription) {
 //        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
 //        LayoutInflater inflater = getLayoutInflater();
